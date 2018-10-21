508c7da075b2392b1e7c75f43a0f4f371d4587a9
Viktor S <mail@schlegel.io>
2018-10-21 20:51:51 +0200

    Removed

--- js/PassageDat.js
+++
@@ -1,7 +0,0 @@
-class PassageDat {
-    constructor(passageTag){
-        this.tag = passageTag;
-        this.section  = passageTag.substring(3,5);
-        this.chapter   = passageTag.substring(1,3);
-    }
-}
